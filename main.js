f983b62117aca42a2210fa1fd38b3f901135f87e
samu10112 <119002488+samu10112@users.noreply.github.com>
2022-12-26 21:33:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+        console.log("essa e uma tela console");
+        var primeiro_nome="William";
+        var ultimo_nome="Hurt";
+        var nome_completo=primeiro_nome.concat(ultimo_nome);
+        console.log(nome_completo);
+        var substituto=nome_completo.replace("William", "Bonner");
+        console.log(substituto);
+        var comprimento=ultimo_nome.length;
+        console.log("William: "+comprimento);
+        var maiuscula=primeiro_nome.toUpperCase();
+        var minuscula=ultimo_nome.toLowerCase();
+        console.log(maiuscula+" "+minuscula);
